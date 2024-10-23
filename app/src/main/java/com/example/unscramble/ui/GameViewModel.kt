b362ab66b59275143ffd0060c3dc24f904a0f9e3
festusrono <festusrono56@gmail.com>
2024-10-23 19:31:33 +0200

    update 1.2

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.unscramble.ui
 
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,3 +13,15 @@ class GameViewModel : ViewModel() {
 private var _count = 0
 val count: Int
     get() = _count
+private lateinit var currentWord: String
+private var usedWords: MutableSet<String> = mutableSetOf()
+private fun pickRandomWordAndShuffle(): String {
+    //continue picking up random words until you get one that hasn't been used
+    currentWord = allWords.random()
+    if (usedWords.contains(currentWord)) {
+        return pickRandomWordsAndShuffle()
+    } else {
+        usedWords.add(currentWord)
+        return shuffleCurrentWord(currentWord)
+    }
+}
